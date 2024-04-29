134e888d9919da4151880ad8f849cf1f415e5599
Dhanush <dhanushkrish54@gmail.com>
2024-04-29 21:19:28 +0530

    29-04-2024 | 09:19PM

---
+++ Training/src/Package2/EmployeeObj1.java
@@ -0,0 +1,29 @@
+package Package2;
+
+public class EmployeeObj1 {
+
+	private int Emp_no;
+	String name;
+	
+	EmployeeObj1(int no, String name){
+		this.Emp_no = no;
+		this.name = name;
+	}
+	
+	public static void main(String[] args) {
+		
+		EmployeeObj1 emp1 = new EmployeeObj1(1, "dhanush");
+		EmployeeObj1 emp2 = new EmployeeObj1(2, "dhan");
+		//EmployeeObj1 emp3 = (EmployeeObj1) emp1.clone(); 
+		emp1.getClass();
+		emp1.hashCode();
+		emp1.toString();
+		System.out.println(emp1.getClass());
+		System.out.println(emp1.hashCode());
+		System.out.println(emp1.toString());
+		System.out.println(emp2.equals(emp1));
+		//System.out.println(emp3.Emp_no);
+		//emp1.finalize();
+	}
+
+}
