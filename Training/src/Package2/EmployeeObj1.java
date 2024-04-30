7ac140f8530716c9f3fc61a230a9347e2c73723c
Dhanush <dhanushkrish54@gmail.com>
2024-04-30 08:38:17 +0530

    30-04-2024 | 08:38AM

--- Training/src/Package2/EmployeeObj1.java
+++ Training/src/Package2/EmployeeObj1.java
@@ -10,20 +10,26 @@ public class EmployeeObj1 {
 		this.name = name;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws CloneNotSupportedException {
 		
+		try {
 		EmployeeObj1 emp1 = new EmployeeObj1(1, "dhanush");
 		EmployeeObj1 emp2 = new EmployeeObj1(2, "dhan");
-		//EmployeeObj1 emp3 = (EmployeeObj1) emp1.clone(); 
+		EmployeeObj1 emp3 = (EmployeeObj1) emp1.clone(); 
 		emp1.getClass();
 		emp1.hashCode();
 		emp1.toString();
+		System.out.println(emp1.Emp_no + " " + emp1.name);
 		System.out.println(emp1.getClass());
 		System.out.println(emp1.hashCode());
 		System.out.println(emp1.toString());
 		System.out.println(emp2.equals(emp1));
 		//System.out.println(emp3.Emp_no);
 		//emp1.finalize();
+		}
+		catch(CloneNotSupportedException c) {
+			System.out.println("Clone Not Supported");
+		}
 	}
 
 }
