f705974254cab17f93042a2e0261d21a716fbc95
dhan-profile <dhanushkrish54@gmail.com>
2024-06-07 13:40:15 +0530

    07-06-2024 | 01:40PM

---
+++ Training/src/Package2/Optional_Ex.java
@@ -0,0 +1,78 @@
+package Package2;
+
+import java.io.*;
+import java.util.*;
+
+public class Optional_Ex {
+	public static void main(String args[]) throws Throwable {
+
+// Optional - prevents nullpointer exception
+		String s = "hello";
+		String s1 = null;
+
+//	        if(s1 != null && s != null)
+//	       System.out.println(s1.concat("hello"));
+//	       
+
+// empty()
+
+//	             Optional<String> op5 = Optional.empty();
+//	             System.out.println(op5);
+
+		/*
+		 * //of() - mandatory give String, does not accept null value
+		 * 
+		 * Optional<String> op = Optional.of(s); System.out.println(op);
+		 */
+
+		/*
+		 * //ofNullable()
+		 * 
+		 * Optional<String> op = Optional.ofNullable(s); System.out.println(op);
+		 * 
+		 * Optional<String> op1 = Optional.ofNullable(s1); System.out.println(op1);
+		 */
+
+		/*
+		 * //isPresent()
+		 * 
+		 * Optional<String> op1 = Optional.ofNullable(s);
+		 * System.out.println(op1.isPresent());
+		 * 
+		 * if(op1.isPresent()){ System.out.println(s.concat(" language")); }
+		 * 
+		 */
+
+		Optional<String> op = Optional.ofNullable(s); // hello
+		Optional<String> op1 = Optional.ofNullable(s1);// null
+
+		/*
+		 * //orElse() //If value is present in container, then the value is return or
+		 * default value will be provided
+		 * 
+		 * System.out.println(op.orElse("there is null"));
+		 * System.out.println(op1.orElse("there is null"));
+		 * 
+		 * //orElseGet() //If value is present in container, then the value is return or
+		 * takes value from the supplier System.out.println(op.orElseGet(() -> new
+		 * String("there is null"))); System.out.println(op1.orElseGet(() -> new
+		 * String("there is null")));
+		 * 
+		 * //orElseThrow() //If value is present in container, then the value is return
+		 * or Exception thrown from the supplier System.out.println(op.orElseThrow(() ->
+		 * new RuntimeException("there is null")));
+		 * System.out.println(op1.orElseThrow(() -> new
+		 * RuntimeException("there is null")));
+		 * 
+		 */
+
+// filter
+//	        System.out.println(op.filter(t->t.contains("l")));
+//	        System.out.println(op1.filter(t->t.contains("s")));
+
+// ifPresent()
+//	        op.ifPresent(value -> System.out.println(value.concat(" welcome")));
+//	       op1.ifPresent(value -> System.out.println(value.concat(" welcome")));
+
+	}
+}
--- Training/src/Package2/Predefined_Interface.java
+++ Training/src/Package2/Predefined_Interface.java
@@ -2,29 +2,33 @@ package Package2;
 import java.util.function.*;
 public class Predefined_Interface {
 //===============================================================
-//Methods in Function Interface - take a single input and produce return value.
+//Function Interface - take a single input and produce return value.
 //===============================================================
 //apply()- input
 //andThen() - first obj input and passed into the sec obj
 //compose() - 
 //identity() - duplicate value
+	
 //===============================================================
-//Predicate - take a single input and give their boolean output.
+//Predicate Interface - take a single input and give their boolean output.
 //===============================================================
 //test()- input
 //and() - both cond satified
 //or() - either one cond satify
 //negate() - inverse 
 //isEqual() - compare
+	
 //===============================================================
 //Supplier Interface - no input and only returns values
 //===============================================================
 //Type get()
+	
 //===============================================================
 //Consumer Interface - take a single input and give their type output(void).
 //===============================================================
 //accept()- input
 //andThen()- same input
+	
 //------------------------------------------------------------------------------
 //Bi-Predicate - take 2 input and give their boolean output.
 //Bi-Consumer - take 2 input and give their result.
@@ -42,5 +46,14 @@ public class Predefined_Interface {
 		
 		Function<String, Integer> function = f -> Integer.parseInt(f);
 		System.out.println(function.apply("123"));
+		
+		Function<Integer, Integer> combined = c1 -> c1+c1;
+		Function<Integer, Integer> combined2 = c2 -> c2*c2;
+		System.out.println(combined2.andThen(combined).apply(10));
+		System.out.println(combined2.compose(combined).apply(10));
+		
+		Function<Integer, Integer> clone = Function.identity();
+		System.out.println(clone.apply(4));
+		System.out.println(combined2.apply(4));
 	}
 }
