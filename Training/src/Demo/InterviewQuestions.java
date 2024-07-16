8f24a833405ec5608fd7242e2176f13921ef9ba9
dhan-profile <dhanushkrish54@gmail.com>
2024-07-16 13:42:29 +0530

    16-07-2024 | 01:42PM

---
+++ Training/src/Demo/InterviewQuestions.java
@@ -0,0 +1,168 @@
+//Java Technical Interview Questions
+//======================================
+//1. What is JDK, JRE, JVM, and JIT?
+//2. What are variables? What are its types?
+//3. What is the local variable? What is an instance variable?
+//4. Difference between static and instance variable.
+//5. What are data types? How many types?
+//6. Primitive and non-primitive data types.
+//7. What is Java's primitive unsigned integral type?
+//8. What are conditional statements?
+//9. Features of Java. (List all 12 points).
+//10. What are classes and objects?
+//11. Difference between jump and break statements.
+//12. What are methods in Java? What are its types?
+//13. What is method signature?
+//14. How to use a method?
+//15. What is the difference between a method and a function?
+//16. What is method overloading?
+//17. What is a constructor? Types of overloading constructors.
+//18. What is the ‘this’ keyword?
+//19. What is inheritance? What are its types?
+//20. Why is multiple inheritance not supported in java? Why do we use interface?
+//21. What is polymorphism? What are its types?
+//22. What is method overriding?
+//23. Why do we use the final keyword?
+//24. Why do we use super keywords?
+//25. What is abstraction? Abstract classes and methods.
+//26. What is Encapsulation?
+//27. What is Interface?
+//28. What are access modifiers?
+//29. Difference between String and StringBuffer.
+//30. What is an anonymous array?
+//31. What are the types of arrays?
+//32. How many ways to clear garbage in java?
+//33. Why are Strings immutable?
+//34. What is Garbage collection in java?
+//35. What are anonymous objects?
+//36. Explain intern() method?
+//37. What are the 2 types of exceptions?
+//38. What is multithreading?
+//39. What is an exception?
+//40. What is exception handling?
+//41. What are the types of exception handling?
+//42. Difference between error and exception.
+//43. Is it possible to declare a try without a catch block?
+//44. In which order multiple catch blocks have to be given?
+//45. Difference between throw and throws keyword?
+//46. Difference between final, finally and finalize.
+//47. What is a null pointer exception?
+//48. What is a number format exception?
+//49. What are the rules / How to create a user defined exception?
+//50. Difference between a thread and a process.
+//51. What are the ways to implement thread?
+//52. What are the different states of thread?
+//53. What is the start() method?
+//54. Is it possible to execute the same thread again?
+//55. What is the priority of a thread scheduler?
+//56. Difference between daemon thread and user thread.
+//57. Difference between wait() and sleep().
+//58. What is a package?
+//59. What is ArrayList?
+//60. What is the latest version of java?
+//61. Difference between constructor and method.
+//62. Difference between instance variable and class variable.
+//63. What is multilevel inheritance? (A -> B -> C)
+//64. Is it possible to call a constructor in a method?
+//65. Difference between this keyword and super keyword.
+//66. Is it possible to declare a variable in an interface?
+//67. Is it possible to declare a constructor in an interface?
+//68. Is it possible to have a private abstract method in an abstract class?
+//69. Why do we use access modifiers?
+//70. Is it possible to access parent class members using upcasting and override methods?
+//71. Difference between method overloading and overriding.
+//72. Why is the static method not overridden?
+//73. Why do we use getter and setter methods?
+//74. Difference between inheritance and aggregation.
+//75. Is it possible to overload constructor? method?
+//76. Difference between equals() method and == operator.
+//77. What is the String intern() method?
+//78. Difference between String buffer and String builder.
+//79. Difference between concat operator ‘+’ and method.
+//80. What are the advantages of multithreading?
+//81. Difference between wait() and sleep() methods.
+//82. List some object class methods.
+//83. Why does stack overflow exception occur?
+//84. What is a synchronized keyword?
+//85. What is a deadlock?
+//86. Difference between notify() and notifyall().
+//87. What is late binding / dynamic binding?
+//88. What are the benefits of inner class?
+//89. What is a volatile keyword?
+//90. What is static binding?
+//91. Difference between static binding and dynamic binding.
+//92. Difference between upcasting and downcasting.
+//93. What is an Iterable interface?
+//94. What is an iterator?
+//95. What is ArrayList?
+//96. What is Vector?
+//97. What is stack?
+//98. What is Queue?
+//99. What is Queue/PriorityQueue?
+//100. What is Deque/ArrayDeque?
+//101. What is a LinkedList?
+//102. What is Hashset?
+//103. What is a Linked HashSet?
+//104. What is a SortedSet?
+//105. What is a TreeSet?
+//106. Difference between Collection and Collections.
+//107. Difference between Collection and List interface.
+//108. Difference between Arrays and ArrayList.
+//109. Difference between ArrayList and LinkedList.
+//110. Difference between ArrayList and Vector.
+//111. Difference between HashSet and TreeSet.
+//112. Can you increase the heap memory of JVM? or What is the OutofMemoryException? How can we handle it?
+//113. Can you override private or static methods in Java?
+//114. Does finally (block) always execute in Java?
+//115. How to make a class immutable?
+//116. Difference between Iterator and ListIterator.
+//117. What is the default size of the load factor in hashing based collection?
+//118. What is the difference between an Iterator and an Enumeration?
+//119. What is a priority queue in Java?
+//120. What is hashCode()?
+//121. What is the difference between Comparable and Comparator?
+//122. Why do we override equals() method?
+//123. How to convert ArrayList to Array and Array to ArrayList?
+//124. What is a Collection(framework)?
+//125. What is HashMap?
+//126. What is LinkedHashMap?
+//127. Difference between Map and Set.
+//128. Explain some java util class methods.
+//129. What is ConcurrentModificationException?
+//130. What are non-access modifiers?
+//131. What is the job of a thread scheduler?
+//132. What is a transient modifier/keyword?
+//133. Can we declare the constructor final?
+//134. What is Thread safe?
+//135. Difference between join() and yield().
+//136. What are generics?
+//137. Unary operator?
+//138. What is Static initializer and instance initializer?
+//139. What are the features in Java 8?
+//140. What is a functional interface?
+//141. What is a lambda expression?
+//142. What are the advantages of Lambda expression?
+//143. Difference between anonymous class and lambda expressions.
+//144. What are default methods in an interface?
+//145. What are static methods in an interface?
+//146. What is method reference?
+//147. What is Optional class?
+//148. Which method of Optional class does not accept NULL?
+//149. What is the DateTime API?
+//150. What is the Predicate interface in Java 8?
+//151. Difference between Consumer and Supplier.
+//152. Difference between Predicate and Function.
+//153. List a few functional interfaces which are already there before Java 8?
+//154. Difference between Predicate and BiPredicate.
+//155. How many ways can you create an object for a class in java?
+//156. What is Stream API?
+//157. What is a pipeline?
+//158. How does Stream work internally?
+//159. What are parallel streams?
+//160. What are intermediate operations?
+//161. Mention some intermediate operations methods.
+//162. Mention some terminal operations methods.
+//163. Difference between abstract classes and interfaces in Java.
+//164. Write a code snippet to reverse a linked list in Java.
+//165. Describe the MVC (Model-View-Controller) architecture pattern and its advantages.
+//166. How would you optimize the performance of a web application?
